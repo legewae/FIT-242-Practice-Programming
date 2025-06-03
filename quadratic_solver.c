ccc7633e48bc5ad131f9a61a4950cb4141d5d0fa
legewae <55580266+legewae@users.noreply.github.com>
2025-06-03 15:16:47 +0600

    chore: Remove broken comments

--- quadratic_solver.c
+++ quadratic_solver.c
@@ -7,27 +7,26 @@ int quadratic_solver(double a, double b, double c, double eps, double **roots){
 
    if(fabs(a)<eps) {
       if(roots) *roots = NULL;
-	  return NOT_QUADRATIC_EQUATION; //Не является квадратичным уравнением
+	  return NOT_QUADRATIC_EQUATION;
    }
 
-   //Каждый коэффициент делим для легкости вычисления
+  
    b = b / a;
    c = c / a;
 
-   double disc = b * b - 4 * c; //Дискриминант
+   double disc = b * b - 4 * c;
    if(disc < -eps) {
 	  *roots = NULL;
-	  return NO_ROOTS; //Нет корней
-   } else if(fabs(disc) <= eps) { //Дискриминант равен(почти) нулю
-	  *roots = (double *)malloc(1  *sizeof(double)); //Корень 1, выделяем память на 1
-	  (*roots)[0] = -b / 2; //Один корень
+	  return NO_ROOTS;
+   } else if(fabs(disc) <= eps) {
+	  *roots = (double *)malloc(1  *sizeof(double));
+	  (*roots)[0] = -b / 2;
 	  return ONE_ROOT;
    } else {
-	  *roots = (double *)malloc(2 * sizeof(double)); //Корня 2, выделяем память на 2
+	  *roots = (double *)malloc(2 * sizeof(double));
 
-	  (*roots)[0] = (-b - sqrt(disc)) / 2; //Первый корень
-	  (*roots)[1] = (-b + sqrt(disc)) / 2; //Второй корень
-	  //Корни по возрастанию
+	  (*roots)[0] = (-b - sqrt(disc)) / 2;
+	  (*roots)[1] = (-b + sqrt(disc)) / 2;
 	  return TWO_ROOTS;
    }
 }
