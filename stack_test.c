c4dc745df86ff88771e04a24ee90d2ebf8235ff6
legewae <55580266+legewae@users.noreply.github.com>
2025-06-11 23:34:52 +0600

    fix: stack tests

--- stack_test.c
+++ stack_test.c
@@ -4,66 +4,60 @@
 #include "stack.h"
 
 void test_stack_create() {
-    stack* st = stack_create();
+    stack* st = stack_create(100);
     assert(st != NULL);
     assert(stack_is_empty(st));
     stack_free(st);
 }
 
 void test_stack_push() {
-    stack* st = stack_create();
+    stack* st = stack_create(100);
     int success = 0;
     
     assert(stack_push(st, 1) == SUCCESS);
-    assert(stack_peek(st, &success) == 1);
-    assert(success == SUCCESS);
-    
-    assert(stack_pop(st, &success) == 1);
-    assert(success == SUCCESS);
+	int value = 0;
+    assert(stack_peek(st, &value) == SUCCESS);
+    assert(value == 1);
+    value = 0;
+    assert(stack_pop(st, &value) == SUCCESS);
+	assert(value == 1);
     assert(stack_is_empty(st));
     stack_free(st);
 }
 
 void test_stack_peek_empty() {
-    stack* st = stack_create();
-    int success = SUCCESS;
+    stack* st = stack_create(10);
+    int value = 0;
     
-    assert(stack_peek(st, &success) == 0);
-    assert(success == ERROR);
-    stack_free(st);
-}
-
-void test_stack_increase() {
-    stack* st = stack_create();
-    assert(stack_increase(st) == SUCCESS);
-    assert(st->size == DEFAULT_SIZE + PLUS_SIZE);
+    assert(stack_peek(st, &value) == ERROR);
+    assert(value == 0);
     stack_free(st);
 }
 
 void test_stack_overfilled() {
-    stack* st = stack_create();
+    stack* st = stack_create(10);
     int success = 0;
 
-    for (int i = 0; i <= DEFAULT_SIZE; i++) {
+    for (int i = 0; i < st->size; i++) {
         assert(stack_push(st, i) == SUCCESS);
     }
+    
 
-    assert(stack_pop(st, &success) == DEFAULT_SIZE);
-    assert(success == SUCCESS);
+    assert(stack_push(st, 123) == STACK_OVERFLOW);
     stack_free(st);
 }
 
 void test_stack_push_and_pop() {
-    stack* st = stack_create();
-    int success = 0;
+    stack* st = stack_create(101);
+    int value = 0;
     
     for (int i = 0; i <= 100; i++) {
         assert(stack_push(st, i) == SUCCESS);
     }
 
     for (int i = 100; i >= 0; i--) {
-        assert(stack_pop(st, &success) == i);
-        assert(success == SUCCESS);
+        assert(stack_pop(st, &value) == SUCCESS);
+		assert(value == i);
     }
 
     assert(stack_is_empty(st));
@@ -74,7 +68,6 @@ int main() {
     test_stack_create();
     test_stack_push();
     test_stack_peek_empty();
-    test_stack_increase();
     test_stack_overfilled();
     test_stack_push_and_pop();
     return 0;
