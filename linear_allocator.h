5b7c4600876af066f6a6dbc670337c9f0dd15ad1
legewae <55580266+legewae@users.noreply.github.com>
2025-06-11 15:42:32 +0600

    chore: add end of file

--- linear_allocator.h
+++ linear_allocator.h
@@ -11,3 +11,4 @@ linear_allocator* linear_allocator_init(size_t size);
 void linear_allocator_free(linear_allocator* allocator);
 void linear_allocator_reset(linear_allocator* allocator);
 void* linear_alloc(linear_allocator* allocator, size_t size);
+
